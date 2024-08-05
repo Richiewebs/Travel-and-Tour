db8b6f3c91e2ef9ab1198f2d2f0a43b42c1d3acf
Richiewebs <asmahrichelle9@gmail.com>
2024-08-05 13:52:32 +0000

    small changes

--- src/Pages/Service.jsx
+++ src/Pages/Service.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
-import beach from '../assets/house.jpg'
+import house from '../assets/house.jpg'
 import App from '../components/Accordion'
 
 const Service = () => {
   return (
     <div className='flex flex-col w-full p-10 pt-14 bg-white' class='service'>
 
-        <h1 className='text-center text-slate-900 text-3xl font-bold'>Services</h1>
+        <h1 className='text-center text-slate-900 text-3xl pt-10 font-bold'>Services</h1>
 
     
         <div className='flex w-full pt-14 '>
-        <div className='w-1/2 '>
+        <div className='w-1/2 px-5'>
             <div className='w-11/12 py-10 p-5 transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 hover:duration-300'>
-                <img src={beach} alt="" srcset="" />
+                <img src={house} alt="" srcset="" />
             </div>
         </div >
 
