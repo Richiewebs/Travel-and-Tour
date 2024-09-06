50f38b48f0455793bb909d8e5375e6b2a6647cbc
Richiewebs <asmahrichelle9@gmail.com>
2024-09-06 00:03:15 +0000

    small changes

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,23 +1,33 @@
-import React from 'react'
+// Home.js
+import React from 'react';
 import Hero from '../components/Hero';
 import Service from './Service';
 import About from './About';
-import Porfolio from './Porfolio';
+import Portfolio from './Porfolio';
 import Contact from './Contact';
 import Footer from '../components/Footer';
 
 const Home = () => {
-   
   return (
     <div className='flex flex-col w-full text-white overflow-hidden'>
-        <Hero></Hero>
-        <Service></Service>
-        <About></About>
-        <Porfolio></Porfolio>
-        <Contact></Contact>
-        <Footer></Footer>
+      <section id="hero">
+        <Hero />
+      </section>
+      <section id="services">
+        <Service />
+      </section>
+      <section id="about">
+        <About />
+      </section>
+      <section id="portfolio">
+        <Portfolio />
+      </section>
+      <section id="contact">
+        <Contact />
+      </section>
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,39 @@
-import React from 'react'
+import React from 'react';
 
 const Navbar = () => {
   return (
     <div className='w-full h-28 transparent absolute flex justify-between cursor-pointer '>
-        <div className='w-10/12 h-24 flex pl-24'>
-            <ul className='flex text-gray-200 space-x-20 p-8 text-lg'>
-                <li className='hover:border-solid hover:border-b-4 hover:text-white'>Home</li>
-                <li className='hover:border-solid hover:border-b-4 hover:text-white'>Services</li>
-                <li className='hover:border-solid hover:border-b-4 hover:text-white'>About us</li>
-                <li className='hover:border-solid hover:border-b-4 hover:text-white'>Porfolio</li>
-                <li className='hover:border-solid hover:border-b-4 hover:text-white'>Contact</li>
-            </ul>
-        </div>
-      
+      <div className='w-10/12 h-24 flex pl-24'>
+        <ul className='flex text-gray-200 space-x-20 p-8 text-lg'>
+          <li>
+            <a href="#hero" className='hover:border-solid hover:border-b-4 hover:text-white'>
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#services" className='hover:border-solid hover:border-b-4 hover:text-white'>
+              Services
+            </a>
+          </li>
+          <li>
+            <a href="#about" className='hover:border-solid hover:border-b-4 hover:text-white'>
+              About Us
+            </a>
+          </li>
+          <li>
+            <a href="#portfolio" className='hover:border-solid hover:border-b-4 hover:text-white'>
+              Portfolio
+            </a>
+          </li>
+          <li>
+            <a href="#contact" className='hover:border-solid hover:border-b-4 hover:text-white'>
+              Contact
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
